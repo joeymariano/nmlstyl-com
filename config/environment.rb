018be3806b69e263fb34c5cb9eda894099281fc8
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-30 19:16:07 -0400

    trying to fix db connection

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ Bundler.require(:default)
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  :database => "db/emails.postgresql"
+  :database => "db55jo8q2ghar9"
 )
 
 require_all 'app'
