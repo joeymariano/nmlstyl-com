01c5727c3d3e5a77a2f7e5f1afba3d0523e3f783
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-30 12:45:25 -0400

    heroku mods

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/emails.sql"
+  :adapter => "postgresql",
+  :database => "db/emails.postgresql"
 )
 
 require_all 'app'
