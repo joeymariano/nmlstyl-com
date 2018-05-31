f3e9925b63b41e35af955f3a7c2d4b68fbaabedf
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-30 21:57:29 -0400

    pleez

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,4 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-dbconfig = YAML::load(File.open('config/database.yml'))
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :database => dbconfig
-)
-
 require_all 'app'
